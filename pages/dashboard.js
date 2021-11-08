69f02137dffd23cfe8535b63e063636c0173c0c5
Adarsh P V <adarshpv9@gmail.com>
2021-11-08 17:34:06 +0530

    add ptnt btn

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,24 +1,14 @@
-let loginpage = function(){
-    let user_field = element(by.id('username'));
-    let pass_field = element(by.name('password'));
-    let login_btn = element(by.css('.mdl-button__ripple-container'));
+let dashpage = function(){
+    let add_btn = element(by.css('.primary'));
 
     this.get = function(url){
         browser.get(url);
     };
 
-    this.username = function(user){
-        user_field.sendKeys(user);
-    };
-
-    this.password = function(pass){
-        pass_field.sendKeys(pass);
-    };
-
-    this.login_button = function(){
-        login_btn.click();
+    this.addp_button = function(){
+        add_btn.click();
     };
 
 };
 
-module.exports = new loginpage();
+module.exports = new dashpage();
--- tests/Basic.js
+++ tests/Basic.js
@@ -1,4 +1,5 @@
 let loginpage = require('../pages/login');
+let dashpage = require('../pages/dashboard');
 
 describe('Basic check',function(){
 
@@ -10,4 +11,10 @@ describe('Basic check',function(){
         loginpage.login_button();
         browser.sleep(2000);
     });
+
+    it('add patient',function(){
+        dashpage.addp_button();
+        browser.sleep(2000);
+    });
+
 })
