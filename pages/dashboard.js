b13156a0188b3800f28ea2dcc33637dc12055011
Adarsh P V <adarshpv9@gmail.com>
2021-11-09 15:21:45 +0530

    helpers - wait

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,11 +1,13 @@
+let helper = require('../helpers/helper');
 let dashpage = function(){
-    let add_btn = element(by.css('.primary'));
+    let add_btn = element(by.xpath("//mdl-button[@class='primary patient-add-btn ng-tns-c5-0 mdl-button ng-star-inserted']"));
 
     this.get = function(url){
         browser.get(url);
     };
 
     this.addp_button = function(){
+        helper.waitForElementpresent(add_btn);
         add_btn.click();
     };
 
