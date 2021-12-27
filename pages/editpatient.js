6fe2260cc0fdad0b2b29ce7739a482f851a7fe83
Adarsh P V <adarshpv9@gmail.com>
2021-12-27 17:28:42 +0530

    edit pat page locators

--- pages/editpatient.js
+++ pages/editpatient.js
@@ -8,7 +8,30 @@ let editpatpage = function(){
     let hid = element(by.xpath("//div[@class='common-card personal-detail-section']//div[2]/mdl-textfield[@class='required-label mdl-textfield is-upgraded mdl-textfield--floating-label ng-untouched ng-pristine ng-valid']//input[@class='mdl-textfield__input ng-untouched ng-pristine ng-valid ng-star-inserted']"));
     let riskcat = element(by.xpath("//div[@class='common-card personal-detail-section']//div[2]/mdl-textfield[@class='required-label mdl-textfield is-upgraded mdl-textfield--floating-label ng-untouched ng-pristine ng-valid']//input[@class='mdl-textfield__input ng-untouched ng-pristine ng-valid ng-star-inserted']"));
     let pgender = element(by.xpath("//div[@class='one selector relation-box']//mdl-radio[1]/span[@class='mdl-radio__ripple-container mdl-ripple--center']"));
-    let plocn = element(by.xpath("")); 
+    let plocn = element(by.xpath("//div[@class='common-card detail-section']/div[@class='common-card-content']/div[1]//input[@class='mdl-textfield__input']")); 
+    let rsourcs = element(by.xpath("//mdl-textfield[@class='dropbtn required-label mdl-textfield is-upgraded mdl-textfield--floating-label ng-untouched ng-pristine ng-valid']//input[@class='mdl-textfield__input ng-untouched ng-pristine ng-valid ng-star-inserted']"));
+    let itxpref = element(by.xpath("//div[@class='common-card detail-section']//div[3]//input[@class='mdl-textfield__input']"));
+    let doc = element(by.xpath("//div[@class='common-card detail-section']//mdl-select[@class='mdl-select mdl-select--floating-label ng-untouched ng-valid ng-dirty']//input[@class='mdl-textfield__input']"));
+    let notes = element(by.xpath("//textarea[@class='mdl-textfield__input ng-untouched ng-pristine ng-valid ng-star-inserted']"));
+    let pfname = element(by.xpath("//div[@class='common-card responsible-party-info-section']/div[@class='common-card-content']/div[2]//mdl-textfield[@class='required-label mdl-textfield is-upgraded mdl-textfield--floating-label ng-untouched ng-pristine ng-valid']//input[@class='mdl-textfield__input ng-untouched ng-pristine ng-valid ng-star-inserted']"));
+    let pmname = element(by.xpath("//input[@class='mdl-textfield__input ng-pristine ng-valid ng-star-inserted ng-touched']"));
+    let plname = element(by.xpath("//div[@class='common-card responsible-party-info-section']//div[3]//mdl-textfield[@class='required-label mdl-textfield is-upgraded mdl-textfield--floating-label ng-untouched ng-pristine ng-valid']//input[@class='mdl-textfield__input ng-untouched ng-pristine ng-valid ng-star-inserted']"));
+    let pdob = element(by.xpath("//div[@class='one common-date-field']//input[@class='mdl-textfield__input ng-untouched ng-pristine ng-valid ng-star-inserted']"));
+    let reln = element(by.xpath("//mdl-select[@class='capitalize-text required-label mdl-select ng-untouched ng-valid ng-dirty']//input[@class='mdl-textfield__input']"));
+    let porelapse = element(by.xpath("//span[@class='mdl-checkbox__ripple-container mdl-ripple--center']"));
+    let relapcandid = element(by.xpath("//mdl-select[@class='mdl-select ng-untouched ng-valid ng-dirty']//input[@class='mdl-textfield__input']"));
+    let saddr1 = element(by.xpath("//div[@class='common-card responsible-party-info-section']//div[7]//mdl-textfield[@class='required-label mdl-textfield is-upgraded mdl-textfield--floating-label ng-untouched ng-pristine ng-valid']//input[@class='mdl-textfield__input ng-untouched ng-pristine ng-valid ng-star-inserted']"));
+    let addr2 = element(by.xpath("//div[@class='common-card responsible-party-info-section']//div[7]//mdl-textfield[@class='mdl-textfield is-upgraded mdl-textfield--floating-label ng-untouched ng-pristine ng-valid']//input[@class='mdl-textfield__input ng-untouched ng-pristine ng-valid ng-star-inserted']"));
+    let city = element(by.xpath("//div[8]//input[@class='mdl-textfield__input ng-untouched ng-pristine ng-valid ng-star-inserted']"));
+    let country = element(by.xpath("//div[8]//input[@class='mdl-textfield__input']"));
+    let state = element(by.xpath("//div[9]//input[@class='mdl-textfield__input']"));
+    let zip = element(by.xpath("//mdl-textfield[@class='required-label mdl-textfield is-upgraded mdl-textfield--floating-label ng-untouched ng-pristine ng-valid ng-star-inserted']//input[@class='mdl-textfield__input ng-untouched ng-pristine ng-valid ng-star-inserted']"));
+    let email = element(by.xpath("//div[@class='common-card responsible-party-info-section']/div[@class='common-card-content']/mdl-textfield[1]//input[@class='mdl-textfield__input ng-untouched ng-pristine ng-valid ng-star-inserted']")); 
+    let cemail = element(by.xpath("//mdl-textfield[2]//input[@class='mdl-textfield__input ng-untouched ng-pristine ng-valid ng-star-inserted']"));
+    let phone = element(by.xpath("//input[@name='phone_number']"));
+    let phtype = element(by.xpath("//mdl-select[@name='phone_number_type']//input[@class='mdl-textfield__input']"));
+    let pcontact = element(by.xpath("//mdl-select[@class='required-label long-text capitalize-text mdl-select mdl-select--floating-label ng-untouched ng-valid ng-dirty']//input[@class='mdl-textfield__input']"));
+    
     this.get = function(url){
         browser.get(url);
     };
